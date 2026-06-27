9bbbf2c019c880440a0216f8f0b97871ee2d889f
rorynk <nk965207@gmail.com>
2026-06-27 20:10:17 +0530

    Fix eslint warnings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState}from "react";
+import { useState } from "react";
 import axios from "axios";
 import Weather from "./components/Weather";
 import Forecast from "./components/Forecast";
